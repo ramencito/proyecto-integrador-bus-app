1d12b51686ad0dc9c7f38c1f0c08b83e3e3e42f7
Nicaib <nicolay.c.ibarra@gmail.com>
2026-08-11 14:03:10 -0700

    Delete Proyecto UPBC-BUS/UPBC-BUS directory

--- Proyecto UPBC-BUS/UPBC-BUS/UPBC-BUS/RegistroUsuarios.sql
+++
@@ -1,8 +0,0 @@
-USE UPBC_Bus_DB;
-GO
---Tabla registros de acceso
-CREATE TABLE Alumnos_Registro (
-    id_registro INT IDENTITY(1,1) PRIMARY KEY,matricula VARCHAR(20) NOT NULL,nombre_alumno VARCHAR(100) NOT NULL,fecha_acceso DATETIME DEFAULT GETDATE());
-GO
-
-select * from Alumnos_Registro;
